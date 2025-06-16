843479e53d6f777bf4e13a12ec18d611375e8b6d
Hicham1970 <devhicham2@gmail.com>
2025-06-16 10:24:02 +0100

    installation des dependences

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navigation } from "@/components/navigation";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
         >
-          <Navigation />
-        {children}
-      </body>
-      </html>
-    </ClerkProvider>
+          <main>{children}</main>
+        </body>
+      
+    </html>
   );
 }
